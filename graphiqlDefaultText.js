c30d767f99ad2f7cdc10dcbf8c2f786bf92ffc1f
Dennis Hodges <fermentationist@gmail.com>
2019-08-04 16:40:53 -0400

    attempting to add default variables to graphiql interface

--- graphiqlDefaultText.js
+++ graphiqlDefaultText.js
@@ -1,112 +1,110 @@
 const graphiqlDefaultText = `
 # press the button to select one of the queries or mutations to run. You can add or edit queries and mutations below.
 
-query findContact{
-    contact (id:11){
-      email
-      firstName
-      lastName
-    }
+query findContact($contactID:ID!){
+  contact (id:$contactID){
+    email
+    firstName
+    lastName
   }
-  
-  query allContacts {
-    contacts {
-      id
-      orgid
-      orgname
-      firstName
-      lastName
-      email
-    }
+}
+
+query allContacts {
+  contacts {
+    id
+    orgid
+    orgname
+    firstName
+    lastName
+    email
   }
-  
-  query findCampaign {
-    campaign(id:1){
-      name
-      type
-      user
-      userid
-    }
+}
+
+query findCampaign ($campaignID:ID!){
+  campaign(id:$campaignID){
+    name
+    type
+    user
+    userid
   }
-  
-  query allCampaigns {
-    campaigns {
-      name
-      type
-      user
-      userid
-    }
+}
+
+query allCampaigns {
+  campaigns {
+    name
+    type
+    user
+    userid
   }
-  
-  query findScore {
-    score(id:1){
-      name
-      descript
-      status
-    }
+}
+
+query findScore {
+  score(id:11){
+    name
+    descript
+    status
   }
-  
-  query allScores {
-    scores{
-      name
-      status
-    }
+}
+
+query allScores {
+  scores{
+    name
+    status
   }
-  
-  query findBranding {
-    branding (id:1){
-      siteName, 
-      siteLogoSmall
-    }
+}
+
+query findBranding {
+  branding (id:1){
+    siteName, 
+    siteLogoSmall
   }
-  
-  query allBrandings {
-    brandings {
-      siteName
-      siteLogoSmall
-    }
+}
+
+query allBrandings {
+  brandings {
+    siteName
+    siteLogoSmall
   }
-  
-  ## Mutations ##
-  
-  mutation createContact {
-    createContact(email: "emptynest@shadypines.com", lastName: "Weston", firstName: "Harry"){
-      id
-      cdate
-      email
-    }
+}
+
+## Mutations ##
+
+mutation createContact {
+  createContact(email: "emptynest@shadypines.com", lastName: "Weston", firstName: "Harry"){
+    id
+    cdate
+    email
   }
-  
-  mutation updateContact {
-    updateContact( email: "sophiap29@shadypines.com", id: 11
-      ){
-      lastName
-      firstName
-      email
-    }
+}
+
+mutation updateContact {
+  updateContact( email: "sophiap29@shadypines.com", id: 11
+	){
+    lastName
+    firstName
+    email
   }
-  
-  mutation deleteContact {
-    deleteContact(id: 27){
-      deleted_at
-    }
+}
+
+mutation deleteContact {
+  deleteContact(id: 27){
+    deleted_at
   }
-  
-  
-  
-  
-  
-  mutation updateBranding {
-    updateBranding(id:1, siteLogoSmall: "https://www.dennis-hodges.com/favicon.ico"){
-      siteLogoSmall
-    }
+}
+
+mutation updateBranding {
+  updateBranding(id:1, siteLogoSmall: "https://www.dennis-hodges.com/favicon.ico"){
+    siteLogoSmall
+    siteName
   }
-  
-  mutation branding {
-    updateBranding(id:1, siteLogoSmall: "https://www.dennis-hodges.com/wrong-link"){
-      siteLogoSmall
-    }
+}
+
+mutation updateBrandingAgain {
+  updateBranding(id:1, siteLogoSmall: "https://www.dennis-hodges.com/wrong-link"){
+    siteLogoSmall
+    siteName
   }
+}
 
 `;
 module.exports = graphiqlDefaultText
--- graphiqlVariables.js
+++ graphiqlVariables.js
@@ -1,7 +1,7 @@
-const graphiqlVariables = {
+const graphiqlVariables = `{
     "contactID": 11,
     "campaignID": 1,
     "scoreID": 1
-  }
+  }`
 
 module.exports = graphiqlVariables;
