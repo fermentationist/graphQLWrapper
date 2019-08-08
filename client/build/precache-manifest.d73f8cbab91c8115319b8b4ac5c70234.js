350a0c2eb2a0cc91fd507512c828ffd8865209c3
Dennis Hodges <fermentationist@gmail.com>
2019-08-08 10:08:24 -0500

    Included build/ folder by removing from .gitignore

---
+++ client/build/precache-manifest.d73f8cbab91c8115319b8b4ac5c70234.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6f8ed8c8b3e4bf8c4be07dbc57f37c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27641627de33bbda04b1",
+    "url": "/static/css/main.e3d46376.chunk.css"
+  },
+  {
+    "revision": "80b4935ba3866d42e215",
+    "url": "/static/js/2.c3b02803.chunk.js"
+  },
+  {
+    "revision": "27641627de33bbda04b1",
+    "url": "/static/js/main.50da6ab9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5ab204b9b95c06640dbefae9a65b1db2",
+    "url": "/static/media/GraphQLLanguageService.js.5ab204b9.flow"
+  },
+  {
+    "revision": "4ce7ba191f7ebee4426768f246b2f0e0",
+    "url": "/static/media/autocompleteUtils.js.4ce7ba19.flow"
+  },
+  {
+    "revision": "7f98f032085704c8943ec2d1925c7c84",
+    "url": "/static/media/getAutocompleteSuggestions.js.7f98f032.flow"
+  },
+  {
+    "revision": "4dbec62f1d8e8417afb9cbd19f1268c3",
+    "url": "/static/media/getDefinition.js.4dbec62f.flow"
+  },
+  {
+    "revision": "65b0979ac23feca49e4411883fd8eaab",
+    "url": "/static/media/getDiagnostics.js.65b0979a.flow"
+  },
+  {
+    "revision": "d94118379d362fc161aa1246bcc14d43",
+    "url": "/static/media/getHoverInformation.js.d9411837.flow"
+  },
+  {
+    "revision": "c04e3998712b37a96f0bfd283fa06b52",
+    "url": "/static/media/getOutline.js.c04e3998.flow"
+  },
+  {
+    "revision": "02c24280c5e4a7eb3c6cfcb079a8f1e3",
+    "url": "/static/media/index.js.02c24280.flow"
+  }
+]);
