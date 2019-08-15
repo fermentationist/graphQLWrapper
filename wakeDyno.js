9b00c0b707fb7258d84191893732bcfc75ee586c
Dennis Hodges <fermentationist@gmail.com>
2019-08-15 11:57:31 -0500

    changed method of obtaining host url from __dirname to path.resolve(__dirname)

--- wakeDyno.js
+++ wakeDyno.js
@@ -1,11 +1,22 @@
 const fetch = require("node-fetch");
-const INTERVAL = 300000;// 5 minutes
-const DYNO_URL = process.env.NODE_ENV === "production" ? __dirname : "http://localhost:4000";
+const path = require("path");
 
+// some constants
+const INTERVAL = 5000;// 5 minutes
+const DYNO_URL = process.env.NODE_ENV === "production" ? path.resolve(__dirname) : "http://localhost:4000";
+console.log("path.dirname:", path.resolve(__dirname));
 // This script will run every 5 minutes to keep the heroku dyno awake and running 
-const wakeDyno = () => (setInterval(() => {
-    console.log(`setInteral called. Will fetch ${DYNO_URL}.`);
-    fetch(DYNO_URL, () => console.log(`Calling ${DYNO_URL}...`));
-}, INTERVAL));
+const wakeDyno = () => {
+    setInterval(() => {
+        try { 
+            console.log(`setInterval called. Will fetch ${DYNO_URL}.`);
+            fetch(DYNO_URL, () => console.log(`Calling ${DYNO_URL}. Dyno is woke.`));
+        }
+        catch (err) {
+            console.log(`Error fetching ${DYNO_URL}`);
+        }
+    }, INTERVAL);
+
+};
 
 module.exports = wakeDyno();
