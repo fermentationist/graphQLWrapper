9b6ae93a2d38176418ea5e6bdaada2925032c9e6
Dennis Hodges <fermentationist@gmail.com>
2019-08-15 12:03:35 -0500

    debugging fetch call in wakeDyno.js

--- wakeDyno.js
+++ wakeDyno.js
@@ -4,9 +4,9 @@ const path = require("path");
 // some constants
 const INTERVAL = 5000;// 5 minutes
 const DYNO_URL = process.env.NODE_ENV === "production" ? path.resolve(__dirname) : "http://localhost:4000";
-console.log("path.dirname:", path.resolve(__dirname));
 // This script will run every 5 minutes to keep the heroku dyno awake and running 
 const wakeDyno = () => {
+    console.log("path.dirname:", path.resolve(__dirname));
     setInterval(() => {
         try { 
             console.log(`setInterval called. Will fetch ${DYNO_URL}.`);
